d5b43f09a913a79ffd64550082e50693f46561b2
Siddharth Choudhary <96943338+IamSiddharthChoudhary@users.noreply.github.com>
2022-08-15 15:25:31 +0530

    Making the set token URI better

--- scripts/AdvancedNFT/set_token_URI.py
+++ scripts/AdvancedNFT/set_token_URI.py
@@ -1,6 +1,14 @@
-import imp
 from brownie import AdvancedNFT
+from scripts.helpfulScripts import getShipType, set_TokenURI
+from scripts.AdvancedNFT.CreateMetaData import URIMapping
 
 
 def main():
     advancedCollectible = AdvancedNFT[-1]
+    numberOfCollectibles = advancedCollectible.tokenCounter()
+    print(f"You've got {numberOfCollectibles} collectibles")
+    for tokenId in range(numberOfCollectibles):
+        shipType = getShipType(advancedCollectible.tokenIDToShip(tokenId))
+        if not advancedCollectible.tokenURI(tokenId).startwith("https:/"):
+            print(f"Setting tokenURI of {tokenId}")
+            set_TokenURI(tokenId, advancedCollectible, URIMapping[shipType])
